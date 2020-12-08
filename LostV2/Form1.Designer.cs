c4be9779a40baee97bf5fadefa1d0147e110f129
Tony Theodoropoulos <theodot@outlook.com>
2020-12-08 09:52:35 -0500

    finished program

--- LostV2/Form1.Designer.cs
+++ LostV2/Form1.Designer.cs
@@ -34,46 +34,52 @@
             this.blueLabel = new System.Windows.Forms.Label();
             this.blueImage = new System.Windows.Forms.PictureBox();
             this.redImage = new System.Windows.Forms.PictureBox();
+            this.imageBox = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.blueImage)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.redImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
             this.SuspendLayout();
             // 
             // outputLabel
             // 
             this.outputLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.outputLabel.ForeColor = System.Drawing.Color.White;
-            this.outputLabel.Location = new System.Drawing.Point(36, 43);
+            this.outputLabel.Location = new System.Drawing.Point(24, 28);
+            this.outputLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.outputLabel.Name = "outputLabel";
-            this.outputLabel.Size = new System.Drawing.Size(509, 396);
+            this.outputLabel.Size = new System.Drawing.Size(339, 65);
             this.outputLabel.TabIndex = 2;
-            this.outputLabel.Text = "label1";
+            this.outputLabel.Text = "You are lost in a forest";
             // 
             // redLabel
             // 
             this.redLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.redLabel.ForeColor = System.Drawing.Color.White;
-            this.redLabel.Location = new System.Drawing.Point(101, 481);
+            this.redLabel.Location = new System.Drawing.Point(67, 313);
+            this.redLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.redLabel.Name = "redLabel";
-            this.redLabel.Size = new System.Drawing.Size(386, 35);
+            this.redLabel.Size = new System.Drawing.Size(257, 23);
             this.redLabel.TabIndex = 3;
-            this.redLabel.Text = "label1";
+            this.redLabel.Text = "Go North";
             // 
             // blueLabel
             // 
             this.blueLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.blueLabel.ForeColor = System.Drawing.Color.White;
-            this.blueLabel.Location = new System.Drawing.Point(101, 537);
+            this.blueLabel.Location = new System.Drawing.Point(67, 349);
+            this.blueLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.blueLabel.Name = "blueLabel";
-            this.blueLabel.Size = new System.Drawing.Size(386, 35);
+            this.blueLabel.Size = new System.Drawing.Size(257, 23);
             this.blueLabel.TabIndex = 4;
-            this.blueLabel.Text = "label1";
+            this.blueLabel.Text = "Go South";
             // 
             // blueImage
             // 
             this.blueImage.Image = global::LostV2.Properties.Resources.blue_50x50;
-            this.blueImage.Location = new System.Drawing.Point(36, 522);
+            this.blueImage.Location = new System.Drawing.Point(24, 339);
+            this.blueImage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.blueImage.Name = "blueImage";
-            this.blueImage.Size = new System.Drawing.Size(59, 50);
+            this.blueImage.Size = new System.Drawing.Size(39, 32);
             this.blueImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.blueImage.TabIndex = 1;
             this.blueImage.TabStop = false;
@@ -81,31 +87,45 @@
             // redImage
             // 
             this.redImage.Image = global::LostV2.Properties.Resources.red_50x50;
-            this.redImage.Location = new System.Drawing.Point(36, 466);
+            this.redImage.Location = new System.Drawing.Point(24, 303);
+            this.redImage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.redImage.Name = "redImage";
-            this.redImage.Size = new System.Drawing.Size(59, 50);
+            this.redImage.Size = new System.Drawing.Size(39, 32);
             this.redImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.redImage.TabIndex = 0;
             this.redImage.TabStop = false;
             // 
+            // imageBox
+            // 
+            this.imageBox.BackgroundImage = global::LostV2.Properties.Resources.deepBrush;
+            this.imageBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.imageBox.Location = new System.Drawing.Point(34, 99);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(313, 174);
+            this.imageBox.TabIndex = 5;
+            this.imageBox.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(579, 649);
+            this.ClientSize = new System.Drawing.Size(386, 422);
+            this.Controls.Add(this.imageBox);
             this.Controls.Add(this.blueLabel);
             this.Controls.Add(this.redLabel);
             this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.blueImage);
             this.Controls.Add(this.redImage);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Lost in a Forest";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.blueImage)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.redImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -117,6 +137,7 @@
         private System.Windows.Forms.Label outputLabel;
         private System.Windows.Forms.Label redLabel;
         private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.PictureBox imageBox;
     }
 }
 
--- LostV2/Form1.cs
+++ LostV2/Form1.cs
@@ -10,7 +10,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;  
+using System.Windows.Forms;
+using System.Media;
 
 namespace LostV2
 {
@@ -34,31 +35,121 @@ namespace LostV2
             /// to the next appropriate scene
             if (e.KeyCode == Keys.M)       //red button press
             {
+                if (scene == 0)
+                {
+                    scene = 1;
+                }
+                else if (scene == 1)
+                {
+                    scene = 3;
+                }
+                else if (scene == 2)
+                {
+                    scene = 0;
+                }
+                else if (scene == 3)
+                {
+                    scene = 0;
+                }
+                else if (scene == 4)
+                {
+                    scene = 5;
+                }
+                else if (scene == 5)
+                {
+                    scene = 0;
+                }
+                else if (scene == 6)
+                {
+                    scene = 0;
+                }
+                else if (scene == 7)
+                {
 
+                }
             }
             else if (e.KeyCode == Keys.B)  //blue button press
             {
+                if (scene == 0)
+                {
+                    scene = 2;
+                }
+                else if (scene == 1)
+                {
+                    scene = 4;
+                }
+                else if (scene == 2)
+                {
+                    scene = 7;
+                }
+                else if (scene == 3)
+                {
+                    scene = 7;
+                }
+                else if (scene == 4)
+                {
+                    scene = 6;
+                }
+                else if (scene == 5)
+                {
+                    scene = 7;
+                }
+                else if (scene == 6)
+                {
+                    scene = 7;
+                }
+                else if (scene == 7)
+                {
 
+                }
             }
 
             /// Display text and game options to screen based on the current scene
             switch (scene)
             {
                 case 0:  //start scene  
+                    outputLabel.Text = "You are lost in a forest";
+                    redLabel.Text = "Go North";
+                    blueLabel.Text = "Go South";
                     break;
-                case 1:   
+                case 1:
+                    imageBox.BackgroundImage = Properties.Resources.forestLake;
+                    SoundPlayer player = new SoundPlayer(Properties.Resources.brook);
+                    player.Play();
+
+                    outputLabel.Text = "You come to a lake. Do you want to take a drink?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
                     break;
                 case 2:
+                    outputLabel.Text = "You fall in a pit and die. \n\nPlay again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
                     break;
                 case 3:
+                    outputLabel.Text = "The water is stagnant. You die of cholera. \n\nPlay again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
                     break;
                 case 4:
+                    outputLabel.Text = "A horse goes by. Do you try and ride it?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
                     break;
                 case 5:
+                    outputLabel.Text = "You tame the horse and ride to safety. \n\nPlay again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
                     break;
                 case 6:
+                    outputLabel.Text = "The horse goes by. You die of lonliness. \n\nPlay again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
                     break;
                 case 7:
+                    outputLabel.Text = "Thank you for playing";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
                     break;
                 default:
                     break;
